6947f1368e4bb044027ef2c587db9629c61070be
Neekity <tc_huang1995@163.com>
2022-01-21 10:19:16 +0800

    33. 搜索旋转排序数组 https://leetcode-cn.com/problems/search-in-rotated-sorted-array/

--- src/middle/arraytag.go
+++ src/middle/arraytag.go
@@ -131,3 +131,27 @@ func NextPermutation(nums []int) []int {
 	}
 	return nums
 }
+func Search(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] == target {
+			return mid
+		}
+		if nums[left] <= nums[mid] {
+			if nums[left] <= target && target < nums[mid] {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		} else {
+			if nums[mid] < target && target <= nums[right] {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		}
+
+	}
+	return -1
+}

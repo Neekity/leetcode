66e530cf43aff28e9eb10d80461b7f1078775a48
Neekity <tc_huang1995@163.com>
2022-01-21 14:26:33 +0800

    35. 搜索插入位置 https://leetcode-cn.com/problems/search-insert-position/

--- src/easy/arraytag.go
+++ src/easy/arraytag.go
@@ -86,3 +86,43 @@ func RemoveElement(nums []int, val int) int {
 	}
 	return pre
 }
+
+func SearchInsert(nums []int, target int) int {
+	return BinarySearch1(nums, target)
+}
+
+func BinarySearch1(nums []int, target int) int {
+	l, r, mid := 0, len(nums), 0
+	for {
+		mid = l + (r-l)/2
+		if nums[mid] < target {
+			l = mid + 1
+		} else if nums[mid] > target {
+			r = mid
+		} else {
+			return mid
+		}
+		if l >= r {
+			break
+		}
+	}
+	return l
+}
+
+func BinarySearch2(nums []int, target int) int {
+	l, r, mid := 0, len(nums)-1, 0
+	for {
+		mid = l + (r-l)/2
+		if nums[mid] < target {
+			l = mid + 1
+		} else if nums[mid] > target {
+			r = mid - 1
+		} else {
+			return mid
+		}
+		if l > r {
+			break
+		}
+	}
+	return l
+}

bc12ae9157eccda02f4f01ead324686b6ffe79ec
Neekity <tc_huang1995@163.com>
2022-02-14 16:50:40 +0800

    39. 组合总和 https://leetcode-cn.com/problems/combination-sum/

--- src/middle/arraytag.go
+++ src/middle/arraytag.go
@@ -212,3 +212,23 @@ func IsValidSudoku(board [][]byte) bool {
 	}
 	return true
 }
+
+func CombinationSum(candidates []int, target int) (result [][]int) {
+	var dfs func(begin int, target int)
+	var curPath []int
+	dfs = func(begin int, target int) {
+		if target == 0 {
+			result = append(result, append([]int{}, curPath...))
+		}
+		if target < 0 {
+			return
+		}
+		for i := begin; i < len(candidates); i++ {
+			curPath = append(curPath, candidates[i])
+			dfs(i, target-candidates[i])
+			curPath = curPath[:len(curPath)-1]
+		}
+	}
+	dfs(0, target)
+	return
+}
